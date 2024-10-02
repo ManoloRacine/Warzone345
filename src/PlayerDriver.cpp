77a915f834f93598e3a3ea0af4e54489b7ad6c74
Jaxon-Alexander <jaxonalexander16@gmail.com>
2024-10-01 20:25:43 -0400

    Update PlayerDriver.cpp

--- src/PlayerDriver.cpp
+++ src/PlayerDriver.cpp
@@ -9,8 +9,13 @@ void testPlayers() {
     // Create some dummy territories and orders
     Territory* territory1 = new Territory("Territory 1");
     Territory* territory2 = new Territory("Territory 2");
-    player.toDefend().push_back(territory1);
-    player.toAttack().push_back(territory2);
+    Territory* territory3 = new Territory("Territory 3");
+    Territory* territory4 = new Territory("Territory 4");
+    vector<Territory*> territories = { {territory1},{territory2} };
+    vector<Territory*> territories2 = { {territory4},{territory3} };
+
+    player.toDefend(territories).push_back(territory1);
+    player.toAttack(territories2).push_back(territory2);
 
     Order* order1 = new Order("Deploy Order");
     player.issueOrder(order1);
