2b2b136a4260b3ab2bb93ea8f101a42092864b71
Nektarios <NektariosZampetoulakis@outlook.com>
2024-10-02 17:44:47 -0400

    cleaned up Maploader method

---
+++ build/CMakeFiles/Warzone345.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Warzone345.dir/main.cpp.obj"
+  "CMakeFiles/Warzone345.dir/main.cpp.obj.d"
+  "CMakeFiles/Warzone345.dir/src/Map/Map.cpp.obj"
+  "CMakeFiles/Warzone345.dir/src/Map/Map.cpp.obj.d"
+  "CMakeFiles/Warzone345.dir/src/Map/MapDriver.cpp.obj"
+  "CMakeFiles/Warzone345.dir/src/Map/MapDriver.cpp.obj.d"
+  "Warzone345.exe"
+  "Warzone345.exe.manifest"
+  "Warzone345.pdb"
+  "libWarzone345.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Warzone345.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Warzone345.dir/main.cpp.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/Warzone345.dir/main.cpp.obj: \
+ C:\Users\Nektarios\Documents\GitHub\Warzone345\main.cpp \
+ C:\Users\Nektarios\Documents\GitHub\Warzone345\src/Map/MapDriver.h
