2f258010a09477428695856cdd2cfabdc90cb436
Ryad <100645955+ryad-all@users.noreply.github.com>
2024-10-02 22:26:18 -0400

    test main

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 
 #include "CardsDriver.h"
 #include "src/GameEngine/GameEngineDriver.h"
+#include "src/Orders/OrderDriver.h"
 
 #include "GameEngineDriver.h"
 
 int main() {
     testCards();
     testGameStates();
+    testOrdersLists();
     return 0;
 }
